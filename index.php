c51300bd8da6bcb42937e16c27da8c30e479820a
Eli Jayson <eligence@gmail.com>
2021-01-15 12:08:41 -0500

    DO MORE ACTS OF GOODNESS AND KINDNESS

--- index.php
+++ index.php
@@ -7,8 +7,10 @@ require_once('MySelf.php');
 
 define('KLAL_YISROEL', \Am\Yisroel::Klal());
 
+
 $MySelf = new \Neshamah\MySelf(KLAL_YISROEL);
 
-if ($MySelf->shouldBoardFlight()) {
-    echo '<h1>Moshiach is Here! Hop on your eagle and get to Yerushalayim!</h1>';
-}
+
+echo $MySelf->shouldBoardFlight() ?
+    '<h1>Moshiach is Here! Hop on your eagle and get to Yerushalayim!</h1>'
+    : '<h1>Do more acts of goodness and kindness to bring Moshiach Now!';
